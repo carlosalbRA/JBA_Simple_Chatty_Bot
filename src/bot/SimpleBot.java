5c0f7704408600cb8389f431d051dd288296aaf8
Carlos Alberto Ruíz Ayala <carlosalb_14@outlook.com>
2020-07-06 22:35:00 -0500

    Stage 1/4
    Hello! What’s your name?

---
+++ src/bot/SimpleBot.java
@@ -0,0 +1,9 @@
+package bot;
+
+public class SimpleBot {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Hashirama.");
+        System.out.println("I was created in 2020.");
+    }
+}
