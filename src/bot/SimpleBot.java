d0f72a7cbe45cdc8a6f638d78729db55f1572fe6
Carlos Alberto Ruíz Ayala <carlosalb_14@outlook.com>
2020-07-08 09:21:19 -0500

    Stage 2/4
    How old are you?

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -1,9 +1,30 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! My name is Hashirama.");
         System.out.println("I was created in 2020.");
+
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder1 = scanner.nextInt();
+        int remainder2 = scanner.nextInt();
+        int remainder3 = scanner.nextInt();
+
+        int age        = (remainder1 * 70 + remainder2 * 21 + remainder3 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 }
